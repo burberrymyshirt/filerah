8d705489bee56a81e6b95d4a1d0b962e62a37bd5
Magnus Larsen <magnushlarsen01@protonmail.com>
2025-07-02 16:53:33 +0200

    initial commit

---
+++ lib/filerah.ex
@@ -0,0 +1,18 @@
+defmodule Filerah do
+  @moduledoc """
+  Documentation for `Filerah`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Filerah.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/filerah_test.exs
@@ -0,0 +1,8 @@
+defmodule FilerahTest do
+  use ExUnit.Case
+  doctest Filerah
+
+  test "greets the world" do
+    assert Filerah.hello() == :world
+  end
+end
